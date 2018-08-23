5acafbcfe0784c91a8bcda6b7b876dfe55c35b3e
jeff <jeffbenvenuti@gmail.com>
2018-08-22 22:50:23 -0400

    WIP: state utils and tests

---
+++ src/lib/blackjack/engine/game-utils/stateUtils.js
@@ -0,0 +1,83 @@
+import Shoe from "../shoe/shoe.js";
+import Hand from "../hand/hand.js";
+
+/*
+state = {
+    options: {
+      minimumBet: betStep,
+      dealerStands: 17,
+    },
+    funds: 1000,
+    bet: betStep,
+    betPlaced: false,
+    shoe: newShoe,
+    dealersHand: new Hand(),
+    playersHands: [new Hand()],
+    activeHand: 0,
+    isPlayersTurn: false,
+    isDealersTurn: false,
+    waitForPlayerClick: false,
+  };
+}
+*/
+
+export function hasStateInLocalStorage() {
+  // check for relevent keys to make sure there is a valid game state in local storage somewhere.
+  // this allows existing gamestates to be overwritten if the game is updated with new needs.
+  return (
+    localStorage.getItem("options.minimumBet") !== null &&
+    localStorage.getItem("funds") !== null &&
+    localStorage.getItem("bet") !== null &&
+    localStorage.getItem("betPlaced") !== null &&
+    localStorage.getItem("shoe") !== null
+  );
+}
+
+export function restoreState() {
+  // restore previous values from local storage
+  const restoredFunds = parseInt(localStorage.getItem("funds"), 10);
+  const restoredBet = parseInt(localStorage.getItem("bet"), 10);
+  const restoredBetPlaced = localStorage.getItem("betPlaced");
+
+  const shoe = new Shoe();
+  const shoeCardsAsString = localStorage.getItem("shoe");
+  const restoredShoe = shoe.restoreFromString(shoeCardsAsString);
+
+  const restoredOptions = {
+    minimumBet: parseInt(localStorage.getItem("options.minimumBet"), 10),
+  };
+
+  // now restore the options object and return like new
+  return {
+    options: restoredOptions,
+    funds: restoredFunds,
+    bet: restoredBet,
+    betPlaced: (restoredBetPlaced === "true"),
+    shoe: restoredShoe,
+    dealersHand: new Hand(),
+    playersHands: [new Hand()],
+    activeHand: 0,
+  };
+}
+
+export function createNewState() {
+  const newShoe = new Shoe(8);
+  newShoe.shuffle();
+
+  return {
+    options: {
+      minimumBet: betStep,
+      dealerStands: 17,
+    },
+    funds: 1000,
+    bet: betStep,
+    betPlaced: false,
+    shoe: newShoe,
+    dealersHand: new Hand(),
+    playersHands: [new Hand()],
+    activeHand: 0,
+    isPlayersTurn: false,
+    isDealersTurn: false,
+    waitForPlayerClick: false,
+  };
+}
---
+++ src/lib/blackjack/engine/game-utils/stateUtils.test.js
@@ -0,0 +1,27 @@
+import {
+  createNewState,
+  hasStateInLocalStorage,
+  restoreState,
+} from "./stateUtils";
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+// createNewState()
+it("createNewState() returns default state", () => {
+
+});
+
+// hasStateInLocalStorage()
+it("hasStateInLocalStorage() returns true if valid state stored in local storage", () => {
+
+});
+
+it("hasStateInLocalStorage() returns false if no valid state stored in local storage", () => {
+
+});
+
+it("hasStateInLocalStorage() returns false if incomplete state stored in local storage", () => {
+
+});
